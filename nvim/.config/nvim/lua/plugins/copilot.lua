b41a3317a9778edc7174927a3916e10524521a03
jljl1337 <lckjack123@gmail.com>
2026-08-13 22:40:28 -0600

    feat: disable copilot for env files in nvim

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -9,7 +9,18 @@ return {
                 keymap = {
                     accept = "<Tab>",
                 }
-            }
+            },
+            filetypes = {
+                -- disable for .env files
+                sh = function()
+                    local filename = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+                    if string.find(filename, ".env", 1, true) then
+                        return false
+                    end
+                    return true
+                end,
+                ["*"] = true,
+            },
         })
     end,
 }
